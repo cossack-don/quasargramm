f6e4fdf5fb276bc68e26c6a1754695f76a862aa1
Sergei <ser90593696@yandex.ru>
2021-02-02 14:19:03 +0300

    add breakpoints dek and mobile, add item post, desing home page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,32 +1,46 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+
+    <!-- Header -->
     <q-header class="bg-white text-grey-10" bordered>
       <q-toolbar class="constrain">
-        <q-btn class="large-screen-only q-mr-sm" to="/camera" flat round dense icon="eva-camera-outline" size="18px" />
-<q-separator vertical spaced class="large-screen-only" />
+<!-- btn -->
+        <q-btn class="large-screen-only q-mr-sm"
+         to="/camera" flat round dense 
+         icon="eva-camera-outline" size="18px" />
 
+        <q-separator vertical spaced class="large-screen-only" />
+<!-- title app -->
         <q-toolbar-title class="text-grand-hotel text-bold">
           Quasargramm App
         </q-toolbar-title>
-<q-btn class="large-screen-only" to="/" flat round dense icon="eva-home-outline" size="18px" />
+<!-- btn -->
+        <q-btn class="large-screen-only"
+        to="/" flat round dense 
+        icon="eva-home-outline" size="18px" />
         
       </q-toolbar>
     </q-header>
+<!-- end Header -->
 
+<!-- footer -->
     <q-footer class="small-screen-only bg-white" bordered>
-    
-                <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
+    <!-- all tabs -->
+        <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
+          <!-- tab-item icon-route -->
         <q-route-tab to="/" icon="eva-home-outline" />
         <q-route-tab to="/camera" icon="eva-camera-outline" />
-      
-      </q-tabs>
-      
-      </q-footer>
 
+        </q-tabs>
+      
+    </q-footer>
+<!-- end footer -->
 
-    <q-page-container>
+<!-- main content pages -->
+    <q-page-container class="bg-grey-1">
       <router-view />
     </q-page-container>
+<!-- end main content pages -->
   </q-layout>
 </template>
 
@@ -45,6 +59,7 @@ export default {
 </script>
 
 <style lang="sass">
+
 .q-toolbar
   @media (min-width: $breakpoint-sm-min) 
     height: 77px
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,112 @@
 <template>
-  <q-page class="flex flex-center">
-<h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+
+  <div class="row q-col-gutter-lg">
+    <!-- COL 8 -->
+    <div class="col-12 col-sm-8">
+      <q-card class="card-post q-mb-md" flat bordered v-for="post in posts" :key="post.id">
+
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-bold">sergey__zhur</q-item-label>
+                <q-item-label caption>
+                  {{post.location}}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-img
+              :src="post.imageUrl"
+            />
+
+            <q-card-section>
+              <div class="text-h6">{{post.caption}}</div>
+              <div class="text-caption text-grey">{{post.date | niceDate}}</div>
+            </q-card-section>
+
+          </q-card>
+
+    </div>
+    <!-- COL 4 -->
+    <div class="col-4 large-screen-only">
+      <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">sergey__zhur</q-item-label>
+            <q-item-label caption>
+              Danny Connel
+            </q-item-label>
+          </q-item-section>
+    </q-item>
+    </div>
+  </div>
+
+
   </q-page>
 </template>
 
 <script>
+import { date } from 'quasar'
+
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id:1,
+          caption: 'Golden Gate Bridge',
+          date:1612262884923,
+          location: 'San Franciso, US',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:2,
+          caption: 'Golden Gate Bridge',
+          date:1612262884923,
+          location: 'San Franciso, US',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:3,
+          caption: 'Golden Gate Bridge',
+          date:1612262884923,
+          location: 'San Franciso, US',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:4,
+          caption: 'Golden Gate Bridge',
+          date:1612262884923,
+          location: 'San Franciso, US',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+      ]
+    }
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D h:mmA')
+    }
+  }
 }
 </script>
+
+<style lang="sass">
+.card-post
+  .q-img
+    min-height: 200px
+</style>
